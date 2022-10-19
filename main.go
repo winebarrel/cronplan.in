2b0e1fcd0ed934684b74d5e7336bb9e6514bc222
winebarrel <sugawara@winebarrel.jp>
2022-10-19 19:10:40 +0900

    fix usage

--- main.go
+++ main.go
@@ -54,6 +54,8 @@ USAGE:
   curl %s -G --data-urlencode 'e=5 0 * * ? *'
 
 see http://%s?e=5+0+*+*+?+*
+
+implemented by https://github.com/winebarrel/cronplan
 `, c.Request.Host, c.Request.Host, c.Request.Host))
 		}
 	})
